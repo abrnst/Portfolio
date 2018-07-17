011acbd514245501c03442911471d985c1579a6f
Andrew Berndt <abrnst@gmail.com>
2018-07-17 16:23:51 -0500

    navbar

---
+++ assets/js/js.js
@@ -0,0 +1,12 @@
+function nav() {
+    var x = document.getElementById("nav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+nav();
+
+
+
